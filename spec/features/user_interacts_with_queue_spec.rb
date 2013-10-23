224304675588635d1f291cb76bd5152fb0a9f9c6
Paul Sandhu <spaulsandhu@gmail.com>
2013-10-23 16:38:30 -0500

    mod 6 complete

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -6,11 +6,11 @@ feature "User signs in and interacts with the queue" do
 
   scenario "user visits front page and goes to the sign in page" do
 	  visit root_path
-	  click_link('Sign In')
+	  click_link("Sign In")
 	  expect(page).to have_content "Email Address"
   end
   
-  scenario "user signs in and then adds videos to the queue" do
+  scenario "user signs in and then adds a video to their queue" do
     comedy = Fabricate(:genre)
     futurama = Fabricate(:video, name: "Futurama")  
     family_guy = Fabricate(:video, name: "Family Guy")
@@ -22,9 +22,31 @@ feature "User signs in and interacts with the queue" do
     
     sign_in(simon)
     
-    click_on 
-    find("a[href='/videos/#{futurama.id}']").click
-    expect(page).to have_content(futurama.name)
+    add_video_to_queue(futurama) # find in the macros
+    expect(page).to have_content("Futurama")
+    
+    visit root_path
+
+    add_video_to_queue(family_guy)
+    click_link "My Queue"
+    expect(page).to have_content("Family Guy")    
+
+    visit root_path
+    add_video_to_queue(transformers)
+    expect(page).to have_content("Already in Queue") # Already in Queue button should display here
+    
+    click_link "My Queue"        
+
+    fill_in("box_#{futurama.id}", :with => 3)
+    fill_in("box_#{family_guy.id}", :with => 1)
+    fill_in("box_#{transformers.id}", :with => 2)
+    
+    click_on "Update Instant Queue"
+     
+    expect(find_field("box_#{futurama.id}").value).to eq("3")
+    expect(find_field("box_#{transformers.id}").value).to eq("2")
+    expect(find_field("box_#{family_guy.id}").value).to eq("1")
+    
   end
   
 	
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -5,3 +5,8 @@ def sign_in(a_user=nil)
   fill_in "password", with: user.password
   click_button "Sign in"
 end
+
+def add_video_to_queue(video)
+  click_on("video_#{video.id}") #click the video link, then we're routed to the video show page
+  click_on("Add to My Queue") #add video to the queue
+end
