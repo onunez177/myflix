f2de53a5e2c552630ab55b832be19b0b309d8f81
Paul Sandhu <spaulsandhu@gmail.com>
2013-10-24 15:53:06 -0500

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130915211352) do
+ActiveRecord::Schema.define(version: 20131024205030) do
 
   create_table "genres", force: true do |t|
     t.string "name"
