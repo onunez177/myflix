59ac12da050c1217ec6218d1ce8bff54aa44bd7b
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-07 17:12:43 -0600

    added social networking spec

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.0'
 gem 'haml-rails'
-gem 'bootstrap-sass'
+gem 'bootstrap-sass', '2.3.2.2'
 gem 'bootstrap_form'
 
 
---
+++ spec/features/user_social_networking_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'pry'
+
+
+feature "User interacts with social networking features" do 
+  given (:simon) { Fabricate(:user) }
+  
+  
+  scenario "User adds a follower, and then unfollows them" do
+    paul = Fabricate(:user)
+    
+    comedy = Fabricate (:genre)
+    futurama = Fabricate(:video)
+    futurama.genres << comedy
+
+    review = Fabricate(:review, video: futurama, creator: paul)
+
+    sign_in(simon)
+    
+    click_on("video_#{futurama.id}")
+    expect(page).to have_content(paul.full_name)
+
+    click_on("#{paul.full_name}")
+    expect(page).to have_content("Follow")
+
+    click_link "Follow"
+    expect(simon.following.count).to eq(1)
+    
+    visit people_path
+    expect(page).to have_content("#{paul.full_name}")
+    
+    click_on("del_#{paul.full_name}")
+    expect(page).not_to have_content("#{paul.full_name}")
+  end
+end
