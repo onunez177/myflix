55b0fb25526f7e88b4511a506c8a944a073f27a0
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-08 14:35:09 -0600

    registration email tests complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,7 +35,24 @@ describe UsersController do
       it "sets the @user variable" do
         expect(assigns(:user)).to be_instance_of(User)
       end
-
     end 
+    
+    context "sending emails" do
+      after { ActionMailer::Base.deliveries.clear } #clear the deliveries queue to a blank state to ensure tests are accurate
+
+      it "sends out an email to the user with valid inputs" do
+        post :create, user: {full_name: 'Simon Sandhu', email: 'simon@gmail.com', password: 'password'}
+        expect(ActionMailer::Base.deliveries.last.to).to eq(['simon@gmail.com'])
+      end
+      it "sends out an email containing the user's name with valid inputs" do
+        post :create, user: {full_name: 'Simon Sandhu', email: 'simon@gmail.com', password: 'password'}
+        expect(ActionMailer::Base.deliveries.last.body).to have_content('Simon Sandhu')
+      end
+      it "does not send out an email with invalid inputs" do
+        post :create, user: {full_name: 'Simon Sandhu', email: 'simon@gmail.com', password: ''}
+        expect(ActionMailer::Base.deliveries.last).to eq(nil)
+      end
+    end   
+ 
   end
 end
