1afd0a3634b2ca8250dea1e1d1ef72fde1263afe
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-15 06:25:14 -0600

    minor change to production email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Myflix::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "ps-myflix.heroku.com" }
+  
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
