1708969d0aa6551f8f4d362024e177cbd33d40d5
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-15 00:05:28 -0600

    re-wrote user joins through invitation email spec

--- spec/features/user_invites_a_friend_spec.rb
+++ spec/features/user_invites_a_friend_spec.rb
@@ -4,19 +4,22 @@ require 'pry'
 feature "User invites a friend, and friend joins the site" do
   given (:simon) { Fabricate(:user) }
   
-  scenario "Friend sends email and user joins site" do
-	  clear_emails # because our tests run in random order, we want to clear email queue
+  before do
+    clear_emails # because our tests run in random order, we want to clear email queue
    
     sign_in(simon)
-	  click_link "Invite"
-	  
-	  fill_in "Friend's Name", with: "Paul Sandhu"
-	  fill_in "Friend's Email Address", with: "test@test.com"
-	  fill_in "Invitation Message", with: "You should join this awesome site!"
-	  click_on "Send Invite"
+    click_link "Invite"
+     
+    fill_in "Friend's Name", with: "Paul Sandhu"
+    fill_in "Friend's Email Address", with: "test@test.com"
+    fill_in "Invitation Message", with: "You should join this awesome site!"
+    click_on "Send Invite"
        
-    visit logout_path
-    
+    visit logout_path 
+  end
+  
+  scenario "User joins site through invitation email" do
+	 
     open_email("test@test.com")
     expect(current_email).to have_content("Click here to sign up!")
     
@@ -27,14 +30,15 @@ feature "User invites a friend, and friend joins the site" do
     expect(page).to have_content("You've successfully registered, please log in.")
     
     paul = User.find_by_email("test@test.com")
-    # sign_in(paul)
-    # visit people_path
-
-    sign_in(simon)
-    visit people_path
     
-    expect(page).to have_content(paul.full_name)
-    # expect(page).to have_content(simon.full_name)
+    visit login_path
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with: "password"
+    click_button "Sign in"
+
+    visit people_path
+    expect(page).to have_content(simon.full_name)
+    
    end
 end
 
