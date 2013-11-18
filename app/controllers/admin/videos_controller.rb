76cfef39053051d4723eeaf4b87b0e0034c08e63
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-17 18:07:43 -0600

    admin can add videos

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -9,13 +9,21 @@ class Admin::VideosController < AdminsController
   end
 
 	def create
-	  
+	  @video = Video.new(videos_params)
+    if @video.save
+      @video.genres << Genre.find(params[:genres])  
+      flash[:success] = "Added video #{video.name}!"
+      redirect_to new_admin_video_path
+    else
+      flash[:error] = "Error adding video, please check your input."
+      render :new
+    end
   end
 
 
   private
   
-  def video_params
-  
+  def videos_params
+    params.require(:video).permit(:name, :description)
   end
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -5,7 +5,7 @@ describe Admin::VideosController do
   describe "GET new" do
     it "sets the @video variable to a new instance of video" do
       simon = Fabricate(:user)
-      set_admin
+      set_admin(simon)
       get :new
       expect(assigns(:video)).to be_instance_of(Video)
     end
@@ -20,15 +20,43 @@ describe Admin::VideosController do
 
   describe "POST create" do
     context "with valid input" do
-      it "creates a new video"
-      it "assigns the correct genre to the video"
-      it "redirects to the add new video page"
-      it "sets the flash message"
+      it "creates a new video" do
+        videos = Video.all
+        set_admin
+        genre = Fabricate(:genre)
+        post :create, video: Fabricate.to_params(:video), genres: genre.id
+        expect(videos.count).to eq(1)      
+      end
+      it "assigns the correct genre to the video" do
+        set_admin
+        genre = Fabricate(:genre)
+        post :create, video: Fabricate.to_params(:video), genres: genre.id
+        expect(Video.first.genres.first).to eq(genre)
+      end
+      it "redirects to the add new video page" do
+        set_admin
+        genre = Fabricate(:genre)
+        post :create, video: Fabricate.to_params(:video), genres: genre.id
+        expect(response).to redirect_to new_admin_video_path
+      end
+      it "sets the flash message" do
+        set_admin
+        genre = Fabricate(:genre)
+        post :create, video: Fabricate.to_params(:video), genres: genre.id
+        expect(flash[:success]).to be_present
+      end
     end
     context "with invalid input" do  
-      it "renders the new template" 
-      it "does not create a new video"
-      it "sets the error message"
+      it "renders the new template" do
+        set_admin
+        post :create, video: {name: '', description: ''}
+        expect(response).to render_template :new
+      end
+      it "does not create a new video" do
+        set_admin
+        post :create, video: {name: '', description: ''}
+        expect(Video.all.count).to eq(0)
+      end
     end
   end
 end
