3ad286f08150c99b70bd45292a91047f9138ab65
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-22 15:21:32 -0600

    updated seeds file and changed a little bit of styling

--- app/models/review.rb
+++ app/models/review.rb
@@ -15,7 +15,3 @@ class Review < ActiveRecord::Base
     average('rating').round(1) # Active Record will return the average value of this column with 1 decimal pt
   end
 end
-
-## TODO
-## Add an error message in the video show view template when a review cannot be saved.
-## Add a validation to ensure a user can only review a movie once.
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,81 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(name: "Futurama", 
+v1 = Video.create(name: "Futurama", 
              description: "Will Hedonism bot ever find true love? Will Bender finally kill all humans? Tune in to find out!", 
              poster_url: File.open("public/uploads/futurama_large.jpg"), 
              small_cover_url: File.open("public/uploads/futurama.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/FuturamaClip.mp4")
-Video.create(name: "Video Game Hangover #47", 
-             description: "On this week's show, we're talking about video game endings: the ones that got it right and the ones that missed the mark. Also: Mulchers, hand cramps, vagina monsters, generic white men, and the importance of carrying enough arrows. The end is nigh!",
-             poster_url: File.open("public/uploads/vgh47_large.jpg"), 
-             small_cover_url: File.open("public/uploads/vgh47.jpg"),
-             video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/VGH47Clip.mp4")
-Video.create(name: "Family Guy", 
+v2 = Video.create(name: "Family Guy", 
              description: "Seth McFarlane is totally overrated.", 
              poster_url: File.open("public/uploads/family_guy_large.jpg"), 
              small_cover_url: File.open("public/uploads/family_guy.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/FamilyGuyClip.mp4")
-Video.create(name: "Video Game Hangover #71", 
+
+v3 = Video.create(name: "Video Game Hangover #47", 
+             description: "On this week's show, we're talking about video game endings: the ones that got it right and the ones that missed the mark. Also: Mulchers, hand cramps, vagina monsters, generic white men, and the importance of carrying enough arrows. The end is nigh!",
+             poster_url: File.open("public/uploads/vgh47_large.jpg"), 
+             small_cover_url: File.open("public/uploads/vgh47.jpg"),
+             video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/VGH47Clip.mp4")
+v4 = Video.create(name: "Video Game Hangover #71", 
              description: "Besides pretty graphics, what do you want from next gen games? On this week's show, we're hoping for evolution beyond 1080p. We've also got awesome new theme music, talk of the Wii U, and reviews of Double Dragon Neon, Darksiders II and Demons' Score.",
              poster_url: File.open("public/uploads/vgh71_large.jpg"), 
              small_cover_url: File.open("public/uploads/vgh71.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/VGH71Clip.mp4")
 
-Video.create(name: "Mass Effect 3", 
+v5 = Video.create(name: "Mass Effect 3", 
              description: "The final chapter in the Mass Effect Saga.", 
              poster_url: File.open("public/uploads/masseffect_large.jpg"), 
              small_cover_url: File.open("public/uploads/masseffect_small.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/MassEffect3Clip.mp4")
-Video.create(name: "Transformers: The Movie", 
+v6 = Video.create(name: "Transformers: The Movie", 
              description: "Autobots vs Decepticons! The greatest combination of 80s robot action, ever!", 
              poster_url: File.open("public/uploads/transformers_large.jpg"), 
              small_cover_url: File.open("public/uploads/transformers_small.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/TransformersClip.mp4")
-Video.create(name: "Breaking Bad", 
+v7 = Video.create(name: "Breaking Bad", 
              description: "The greatest show ever made.", 
              poster_url: File.open("public/uploads/breaking_bad_large.jpg"), 
              small_cover_url: File.open("public/uploads/breaking_bad_small.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/BreakingBadClip.mp4")
-Video.create(name: "Batman: Mask of the Phantasm", 
+v8 = Video.create(name: "Batman: Mask of the Phantasm", 
              description: "Best Batman movie ever made.", 
              poster_url: File.open("public/uploads/batman_large.jpg"), 
              small_cover_url: File.open("public/uploads/batman_small.jpg"),
              video_url: "https://s3-us-west-2.amazonaws.com/paul-myflix/movies/BatmanClip.mp4")
 
+top = Genre.create(name: "Top Picks")
 comedy = Genre.create(name: "Comedy")
-drama = Genre.create(name: "Drama")
-action = Genre.create(name: "Action")
+animated = Genre.create(name: "Animated Favourites")
+games = Genre.create(name: "Video Games")
+vgh = Genre.create(name: "Video Game Hangover")
+Genre.create(name: "British Superiority")
+Genre.create(name: "Muscle Madness")
 
-for counter in (1..4) 
-  Video.find(counter).genres << comedy
-end
+v1.genres << comedy
+v2.genres << comedy
+v3.genres << vgh
+v4.genres << vgh
+v5.genres << games 
+v6.genres << animated
+v7.genres << top
+v8.genres << animated
 
-for counter in (3..6)
-  Video.find(counter).genres << drama
-end
+u1 = User.create(full_name: "Paul Sandhu", email: "spaulsandhu@gmail.com", password: "password", admin: true)
+u2 = User.create(full_name: "Simon Sandhu", email: "simon@test.com", password: "password")
+u3 = User.create(full_name: "Bomba Sandhu", email: "bomba@test.com", password: "password")
+u4 = User.create(full_name: "Luna Sandhu", email: "luna@test.com", password: "password")
 
-for counter in (5..8)
-  Video.find(counter).genres << action
-end
+Review.create(body: "I really like this movie, it's incredible!", rating: 5, creator: u1, video: v8)
+Review.create(body: "This is the greatest show ever made. Top notch storytelling, acting, you name it!", rating: 5, creator: u1, video: v7)
+Review.create(body: "Seth McFarlane can be funny sometimes... but usually not.", rating: 2, creator: u1, video: v2)
 
-User.create(full_name: "Paul Sandhu", email: "spaulsandhu@gmail.com", password: "password", admin: true)
-User.create(full_name: "Simon Sandhu", email: "simon@test.com", password: "password")
-User.create(full_name: "Bomba Sandhu", email: "bomba@test.com", password: "password")
-User.create(full_name: "Luna Sandhu", email: "luna@test.com", password: "password")
+Review.create(body: "I'm a cat, so my opinion shouldn't matter much.", rating: 2, creator: u2, video: v4)
+Review.create(body: "Meow meow, I like this movie a lot!", rating: 4, creator: u2, video: v6)
+Review.create(body: "Once upon a time, a cat wrote a review. This is that time.", rating: 1, creator: u2, video: v1)
+
+Review.create(body: "I have sophisticated taste therefore I like this a lot.", rating: 5, creator: u3, video: v5)
+Review.create(body: "Hmm, not so good... no.", rating: 2, creator: u3, video: v3)
+
+Review.create(body: "I am very sweet! Also this movie rocks my socks!", rating: 4, creator: u4, video: v8)
+Review.create(body: "Yes.", rating: 1, creator: u4, video: v1)
