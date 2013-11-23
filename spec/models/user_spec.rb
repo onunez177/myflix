e8c34ca677eb16042a61163122672cc05ed22481
Paul Sandhu <spaulsandhu@gmail.com>
2013-11-22 19:01:15 -0600

    fixed spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,12 +31,4 @@ describe User do
     
     expect(simon.queued_videos.first.queue_position).to eq(1)
   end
-
-  it "checks to see if the user can follow another user" do
-    simon = Fabricate(:user)
-    
-
-    expect(simon.can_follow?).to eq(false)  
-  
-  end
 end
