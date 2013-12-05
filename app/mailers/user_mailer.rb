4b805e60617f825aebb9b81bdbfdf71428829b14
Paul Sandhu <spaulsandhu@gmail.com>
2013-12-05 10:02:42 -0600

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
 
-	def notify_new_user(user)
+  def notify_new_user(user)
     @user = user
     mail from: 'welcome@myflix.com', to: user.email, subject: "Welcome to MyFlix!"
-	end
+  end
 
   def password_reset(user)
     @user = user  
