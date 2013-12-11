9aa5b61a1ebebe0403c875379772a46d21382a33
Paul Sandhu <spaulsandhu@gmail.com>
2013-12-11 15:02:44 -0600

    fixed a bug with search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#search_button').attr('disabled',true);
     
-    $('#search').keyup(function(){
+    $('#search_term').keyup(function(){
         if($(this).val().length !=0){
             $('#search_button').attr('disabled', false);
         }
